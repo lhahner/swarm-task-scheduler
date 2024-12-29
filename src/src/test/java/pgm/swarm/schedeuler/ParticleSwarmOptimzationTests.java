e498f2ae05842dcc38bcbb51dc5cb48459598790
lhahner <lennart.hahner@gmail.com>
2024-12-29 22:00:35 +0100

    adds test folder structure

---
+++ src/src/test/java/pgm/swarm/schedeuler/ParticleSwarmOptimzationTests.java
@@ -0,0 +1,17 @@
+package pgm.swarm.schedeuler;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class ParticleSwarmOptimzationTests {
+	
+	@BeforeAll
+	public void runtests() {
+		ParticleSwarmOptimzation pso = new ParticleSwarmOptimzation();
+	}
+	
+	@Test
+	public void testevaluateSchedueling() {
+		
+	}
+}
