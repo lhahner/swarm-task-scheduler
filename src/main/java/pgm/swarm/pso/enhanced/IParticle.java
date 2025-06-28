da1c62253c36c2d105514574c755cbfeb543578d
qxz3sys <lennart.hahner@partner.bmwgroup.com>
2025-06-28 23:09:19 +0200

    provides first draft for class structure design for improved pso

---
+++ src/main/java/pgm/swarm/pso/enhanced/IParticle.java
@@ -0,0 +1,11 @@
+package pgm.swarm.pso.enhanced;
+
+import pgm.swarm.Agent;
+import pgm.swarm.pso.core.Particle;
+
+public class IParticle extends Particle implements Agent{
+	/**
+	 * TODO implements the improved particle swarm optimization algorithm
+	 * proposed in @source https://doi.org/10.1007/s12652-023-04541-9
+	 **/
+}
--- src/main/java/pgm/swarm/pso/enhanced/ImprovedParticle.java
+++
@@ -1,7 +0,0 @@
-package pgm.swarm.pso.enhanced;
-
-import pgm.swarm.pso.core.Particle;
-
-public class ImprovedParticle extends Particle{
-
-}
